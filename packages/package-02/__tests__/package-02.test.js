b3d23cdd2309342672b358784d8f3fac2f1172d8
LeoChen <leo_chen@mayohr.com>
2022-06-15 11:37:34 +0800

    remove package 02

--- packages/package-02/__tests__/package-02.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const package02 = require('..');
-
-describe('package-02', () => {
-    it('needs tests');
-});
--- packages/package-02/lib/package-02.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = package02;
-
-function package02() {
-    // TODO
-}
